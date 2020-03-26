7ffeaf30084977572f23232224656de2973d31bd
rioner <rioner2525@gmail.com>
2020-03-26 19:08:05 +0900

    new

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+import pyotp
+import datetime
+import time
+
+def lambda_handler(event, context):
+    
+    test = pyotp.TOTP('base32secret3232')
+    print(test.now())
+    print(int(time.mktime(datetime.datetime.now().timetuple())%30))
